f24498bce08d7c1960d3203c529657b69dfbbec4
ikyih <122755388+ikyih@users.noreply.github.com>
2025-01-30 19:17:46 -0500

    aw

--- dazzledisaster/js/plugins/Remove Weapons and Armors.js
+++
@@ -1,6 +0,0 @@
-// Author : mrcopra
-
-Window_ItemCategory.prototype.makeCommandList = function() {
-    this.addCommand(TextManager.item,    'item');
-    this.addCommand(TextManager.keyItem, 'keyItem');
-};
